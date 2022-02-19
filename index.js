d9f3cbb4a03774932a9657478714e8ff8eda5d0f
Stef Arntz <502348@vistacollege.nl>
2022-02-19 13:19:03 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+
+const TOKEN = "OTQ0MjU4OTAxMzQyMjk0MTA2.Yg-_oQ.bD3OFsZhlqR7jXiaQozMIWe-fzo"
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "ping"){
+        message.reply("pong")
+    }
+})
+
+client.login(TOKEN)
